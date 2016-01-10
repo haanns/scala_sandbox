01618b3cd5e82254b88e14bf327bed8204e2e97c
Hans Li <hans.li@richmond.edu>
2016-01-10 12:21:01 -0800

    adding string replacement, etc

---
+++ src/python_data_testing/radish.py
@@ -0,0 +1,48 @@
+from pprint import pprint
+
+file = open('radishsurvey.txt')
+count = 0
+for line in file:
+  line = line.strip().split(' - ')
+  name, item = line
+  print name + ' voted for ' + item
+  if item == "White Icicle":
+    print name
+    count = count + 1
+x, y, z, k = [1, 2, 3, 20]
+print x
+print y
+print z
+print k
+print count
+
+def count_radish(radish_name):
+  file = open('radishsurvey.txt')
+  count = 0
+  for line in file:
+    line = line.strip().split(' - ')
+    name, item = line
+    if item == radish_name:
+      count = count + 1
+  return count
+
+print count_radish("White Icicle")
+print count_radish("Daikon")
+print count_radish("Sicily Giant radishes")
+
+def count_radish_total():
+  file = open('radishsurvey.txt')
+  count = 0
+  empty_dict = {}
+  for line in file:
+    line = line.strip().split(' - ')
+    name, item = line
+    item_strip = item.strip().lower().replace("  "," ")
+    if item_strip not in empty_dict:
+      empty_dict[item_strip] = 1
+    else:        
+      empty_dict[item_strip] = empty_dict[item_strip]+1
+  return empty_dict
+
+print count_radish_total()
+pprint(count_radish_total())
